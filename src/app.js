af666d0d7b504e670f4a2a5084ba6d7fe336cad2
ravi p <ravi105763@gmail.com>
2024-09-26 21:23:04 +0530

    Exploring get,post,delete and put

--- src/app.js
+++ src/app.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello From  DashBoard !!");
+app.use("/user", (req, res) => {
+  res.send("Hello User How are You");
+});
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "ravi", lastName: "parashanaboin" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data saved Successfully");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Data Updated Successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data Deleted Successfully");
 });
 
 app.use("/hello", (req, res) => {
@@ -13,6 +29,10 @@ app.use("/hello", (req, res) => {
 app.use("/test", (req, res) => {
   res.send("Hello From Server !!");
 });
+
+// app.use("/", (req, res) => {
+//   res.send("Hello From  DashBoard !!");
+// });
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");

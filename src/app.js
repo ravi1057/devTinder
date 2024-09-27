06d8a19c6668b09c52502f1d63884598dbf127bc
ravi p <ravi105763@gmail.com>
2024-09-27 09:23:58 +0530

    Muliple route Handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "ravi", lastName: "parashanaboin" });
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    //Route Handler
+    console.log("Handling Route user 1!!");
+    next()
+    // res.send("Route Handler 1");
+  },
+  (req, res, next) => {
+    console.log("Route Handler user2!!!");
+    // res.send("2nd Response");
+    next();
+  },
+  (req,res,next) => {
+    console.log("Route Handler user3!!!");
+    // res.send("3nd Response");
+    next()
+  },
+  (req,res,next) => {
+    console.log("Route Handler user4!!!");
+    // res.send("4nd Response");
+    next()
+  },
+  (req,res,next) => {
+    console.log("Route Handler user5!!!");
+    res.send("5th Response");
+    // next()
+  }
+
+);
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");

8d4a5bed94d125b2a471afcb926e6f5ec3660674
ravi p <ravi105763@gmail.com>
2024-09-30 13:09:33 +0530

    get APIS

--- src/app.js
+++ src/app.js
@@ -3,16 +3,44 @@ const connectDB = require("./config/database.js");
 const app = express();
 const User = require("./models/user.js");
 
-app.use(express.json())
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   //Create a new instance of user model
-  // console.log(req.body)
-    const user = new User(req.body);
+
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User added Succesfully!");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+//  Get user by email
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//  Feed API - GET /feed  -get all the users from database
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
   }
 });
 

01f01ed1831b2a4efdb5cb0ec3a106880b0dd246
ravi p <ravi105763@gmail.com>
2024-10-02 10:41:23 +0530

    Schema Validation

--- src/app.js
+++ src/app.js
@@ -17,6 +17,23 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Get user by Id
+
+app.get("/user/:id", async (req, res) => {
+  const userId = req.params.id;
+  try {
+    console.log(userId);
+    const user = await User.findById(userId);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 //  Get user by email
 
 app.get("/user", async (req, res) => {
@@ -44,9 +61,48 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+// delete a user from database
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    // const user = await User.findByIdAndDelete({ _id: userId });
+    const user = await User.findByIdAndDelete(userId); //Both are same
+    res.send("User deleted Successfully!");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Update a user from database
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data,{runValidators: true});
+    res.send("User updated Successfully!");
+  } catch (err) {
+    res.status(400).send("Update User Failed:"+err.message);
+  }
+});
+
+//Update a user with emailId
+
+app.patch("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ emailId: userEmail }, data);
+    res.send("User updated with email Successfully!!");
+  } catch (err) {
+    res.status(400).send("Update User Email Failed:"+err.message);
+  }
+});
+
 connectDB()
   .then(() => {
-    console.log("Database connection establieshed successfully");
+    console.log("Database connection established successfully");
     app.listen(7777, () => {
       console.log("Server is successfully listening on port 7777...");
     });
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,51 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    firstName:{
-        type:String
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
     },
-    lastName:{
-        type:String
+    lastName: {
+      type: String,
     },
-    emailId:{
-        type:String
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
     },
-    password:{
-        type:String
+    password: {
+      type: String,
     },
-    age:{
-        type:Number
+    age: {
+      type: Number,
+      min: 18,
     },
-    gender:{
-        type:String
-    }
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://dummyimage.com/600x400/0000ff/fff",
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
-})
-
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
